59cb0aafa5b0b8ef046f48f499a48f07b7be37a3
acimaoo <charismaeka0@gmail.com>
2026-09-01 20:49:04 +0700

    penambahan put dan delete wisata

--- jelajahpo_backend/server.js
+++ jelajahpo_backend/server.js
@@ -46,13 +46,42 @@ app.post('/wisata', (req, res) => {
     db.query(sql, [nama_wisata, deskripsi, harga_tiket, id_kategori], (err, result) => {
         if (err) return res.status(500).json({ error: err.sqlMessage });
         res.json({
-            message: 'Wisata berhasil ditambahkan',
+            message: 'Wisata berhasil ditambahkan!',
             id_wisata: result.insertId
         });
     });
 });
 
-////////////////////// GET KATEGROI ////////////////////////
+//////////////////////// PUT WISATA /////////////////////////
+app.put('/wisata/:id_wisata', (req, res) => {
+    const { id_wisata } = req.params;
+    const { nama_wisata, deskripsi, harga_tiket, id_kategori } = req.body;
+
+    if (!nama_wisata || !harga_tiket) {
+        return res.status(400).json({ message: 'Nama Wisata dan Harga Tiket Wajib Diisi!!' });
+    }
+    if (!deskripsi) {
+        return res.status(400).json({ message: 'Deskripsi Wajib Diisi!!' });
+    }
+
+    const sql = 'UPDATE wisata SET nama_wisata = ?, deskripsi = ?, harga_tiket = ?, id_kategori = ? WHERE id_wisata = ?';
+    db.query(sql, [nama_wisata, deskripsi, harga_tiket, id_kategori, id_wisata], (err, result) => {
+        if (err) return res.status(500).json({ error: err.sqlMessage });
+        res.json({ message: 'Wisata berhasil diupdate!' });
+    });
+});
+
+///////////////////// DELETE WISATA ////////////////////////
+app.delete('/wisata/:id_wisata', (req, res) => {
+    const { id_wisata } = req.params;
+    const sql = 'DELETE FROM wisata WHERE id_wisata = ?';
+    db.query(sql, [id_wisata], (err, result) => {
+        if (err) return res.status(500).json({ error: err.sqlMessage });
+        res.json({ message: 'Wisata berhasil dihapus!' })
+    })
+})
+
+////////////////////// GET KATEGORI ////////////////////////
 app.get('/kategori', (req, res) => {
     const sql = 'SELECT * FROM kategori';
     db.query(sql, (err, results) => {
