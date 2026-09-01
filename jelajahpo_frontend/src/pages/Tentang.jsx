82af49986634c4914add0b74028be91af0840be2
acimaoo <charismaeka0@gmail.com>
2026-09-01 17:20:28 +0700

    membuat halaman tentang

---
+++ jelajahpo_frontend/src/pages/Tentang.jsx
@@ -0,0 +1,14 @@
+// Tentang.jsx
+export default function Tentang() {
+    return (
+        <div>
+            <h2>Tentang Saya</h2>
+            <hr />
+            <p><strong>Nama:</strong> Charisma Eka</p>
+            <p><strong>Kelas:</strong> XII RPL 1</p>
+            <p><strong>Hobi:</strong> Sesuai Mood, Yang penting gerak.</p>
+            <p><strong>Cita-cita:</strong> Keliling Dunia</p>
+            <p><strong>Ingin Mengunjungi:</strong> Semua wisata yang ada di dunia</p>
+        </div>
+    )
+}
